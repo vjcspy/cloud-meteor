d439c03a643a58f4feb3bdc9bd965a4261e0c23e
Viet Luong Quoc <vietlq@smartosc.com>
2017-06-29 18:15:05 +0700

    clientstorage and clientfastorders

--- server/modules/accounts/publications/clientstorages.ts
+++ server/modules/accounts/publications/clientstorages.ts
@@ -3,8 +3,22 @@ import {OM} from "../../../code/Framework/ObjectManager";
 import {Role} from "../api/role";
 import {License} from "../../retails/models/license";
 import {ClientStorages} from "../collections/clientstorages";
+import * as _ from "lodash";
+import * as moment from "moment";
+import {DateTimeHelper} from "../../../code/Framework/DateTimeHelper";
 
 Meteor.publish('client_storages', function () {
+  const allClientStorages = ClientStorages.find().fetch();
+  if (allClientStorages.length > 0) {
+    _.forEach(allClientStorages, (clientStorage) => {
+      let createTime = moment(clientStorage['created_at'], 'YYYY-MM-DD');
+      let currentTime = moment(DateTimeHelper.getCurrentDate(), 'YYYY-MM-DD');
+      let diff = createTime.diff(currentTime, 'days');
+      if (diff > 5){
+        ClientStorages.remove(clientStorage);
+      }
+    });
+  }
   if (!this.userId)
     return;
   const user: User = OM.create<User>(User).loadById(this.userId);
