01cc48d0c86ba077d6076fbf09bb86ea47624397
khoild <khoild@smartosc.com>
2017-10-23 16:09:31 +0700

    - done product

--- server/modules/retails/api/product-interface.ts
+++ server/modules/retails/api/product-interface.ts
@@ -1,21 +1,21 @@
 export interface ProductInterface {
-  _id?: string;
-  code: string;
-  name?: string;
-  additional_data?: {
+    _id?: string;
+    code: string;
+    name?: string;
+    additional_data?: Object;
+    pricings: string[];
+    trial_days: number;
+    nousers: number;
+    versions?: ProductVersion[];
     description?: string;
-  };
-  pricings: string[];
-  trial_days: number;
-  nousers: number;
-  versions?: ProductVersion[];
-  created_at?: Date;
-  updated_at?: Date;
+    created_at?: Date;
+    updated_at?: Date;
 }
+
 export interface ProductVersion {
-  name?: string;
-  version?: string;
-  changelog: string;
-  created_at?: Date;
-  updated_at?: Date;
+    name?: string;
+    version?: string;
+    changelog: string;
+    created_at?: Date;
+    updated_at?: Date;
 }
--- server/modules/retails/collections/products.ts
+++ server/modules/retails/collections/products.ts
@@ -5,45 +5,48 @@ import {DateTimeHelper} from "../../../code/Framework/DateTimeHelper";
 
 export const Products = CollectionMaker.make<ProductInterface>("products",
                                                                new SimpleSchema({
-                                                                 _id: {
-                                                                   type: String,
-                                                                   optional: true
-                                                                 },
-                                                                 code: String,
-                                                                 name: String,
-                                                                 additional_data: {
-                                                                   type: Object,
-                                                                   optional: true
-                                                                 },
-                                                                 'additional_data.description': String,
-                                                                 pricings: {
-                                                                   type: Array
-                                                                 },
-                                                                 trial_days: {
-                                                                   type: Number,
-                                                                   optional: true
-                                                                 },
-                                                                 'pricings.$': String,
-                                                                 versions: [new SimpleSchema({
-                                                                   name: String,
-                                                                   version: String,
-                                                                   changelog: {
-                                                                     type: String,
-                                                                     optional: true
-                                                                   },
-                                                                   created_at: {
-                                                                     type: Date,
-                                                                   },
-                                                                   updated_at: {
-                                                                     type: Date,
-                                                                   },
-                                                                 })],
-                                                                 created_at: {
-                                                                   type: Date,
-                                                                   defaultValue: DateTimeHelper.getCurrentDate()
-                                                                 },
-                                                                 updated_at: {
-                                                                   type: Date,
-                                                                   defaultValue: DateTimeHelper.getCurrentDate()
-                                                                 }
-                                                               }));
+                                                                                    _id: {
+                                                                                        type: String,
+                                                                                        optional: true
+                                                                                    },
+                                                                                    code: String,
+                                                                                    name: String,
+                                                                                    additional_data: {
+                                                                                        type: Object,
+                                                                                        optional: true
+                                                                                    },
+                                                                                    description: {
+                                                                                        type: String,
+                                                                                        optional: true
+                                                                                    },
+                                                                                    pricings: {
+                                                                                        type: Array
+                                                                                    },
+                                                                                    trial_days: {
+                                                                                        type: Number,
+                                                                                        optional: true
+                                                                                    },
+                                                                                    'pricings.$': String,
+                                                                                    versions: [new SimpleSchema({
+                                                                                                                    name: String,
+                                                                                                                    version: String,
+                                                                                                                    changelog: {
+                                                                                                                        type: String,
+                                                                                                                        optional: true
+                                                                                                                    },
+                                                                                                                    created_at: {
+                                                                                                                        type: Date,
+                                                                                                                    },
+                                                                                                                    updated_at: {
+                                                                                                                        type: Date,
+                                                                                                                    },
+                                                                                                                })],
+                                                                                    created_at: {
+                                                                                        type: Date,
+                                                                                        defaultValue: DateTimeHelper.getCurrentDate()
+                                                                                    },
+                                                                                    updated_at: {
+                                                                                        type: Date,
+                                                                                        defaultValue: DateTimeHelper.getCurrentDate()
+                                                                                    }
+                                                                                }));
