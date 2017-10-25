cd5ae4c99756ae4d55f687d276718754b021c1e5
khoild <khoild@smartosc.com>
2017-10-25 13:55:34 +0700

    - add some information license has product

--- server/modules/retail/api/license-interface.ts
+++ server/modules/retail/api/license-interface.ts
@@ -1,60 +1,63 @@
 export interface LicenseInterface {
-  _id?: string;
-  key?: string;
-  status?: number;
-  shop_owner_id?: string;
-  shop_owner_username?: string;
-  current_cashier_increment?: number;
-  has_product?: LicenseHasProductInterface[];
-  has_roles?: LicenseHasRoleInterface[];
-  is_auto_generate?: boolean;
-  created_by?: string;
-  created_at?: Date;
-  updated_at?: Date
+    _id?: string;
+    key?: string;
+    status?: number;
+    shop_owner_id?: string;
+    shop_owner_username?: string;
+    current_cashier_increment?: number;
+    has_product?: LicenseHasProductInterface[];
+    has_roles?: LicenseHasRoleInterface[];
+    is_auto_generate?: boolean;
+    created_by?: string;
+    created_at?: Date;
+    updated_at?: Date
 }
+
 /*
 * invoices (license_id, shop_owner_id,
 * id, description, total_amount, purchased_date, status, payment_method)
 * */
-export interface LicenseHasRoleInterface{
-  code: string;
-  name:string;
-  has_permissions: LicenseHasRoleHasPermissionInterface[];
+export interface LicenseHasRoleInterface {
+    code: string;
+    name: string;
+    has_permissions: LicenseHasRoleHasPermissionInterface[];
 }
 
-export interface LicenseHasRoleHasPermissionInterface{
-  code:string;
-  group: string;
-  permission:string;
-  is_active:boolean;
+export interface LicenseHasRoleHasPermissionInterface {
+    code: string;
+    group: string;
+    permission: string;
+    is_active: boolean;
 }
 
 export interface LicenseHasProductInterface {
-  product_id?: string;
-  base_url?: BaseUrl[];
-  pricing_id?: string;
-  has_user: LicenseHasProductHasUser[];
-  has_invoice: LicenseHasProductHasInvoice[];
-  start_version?: string;
-  purchase_date?: Date;
-  expired_date?: Date;
+    product_id?: string;
+    base_url?: BaseUrl[];
+    pricing_id?: string;
+    pricing_type?: string;
+    isFresh: boolean; // has already activated trial
+    has_user: LicenseHasProductHasUser[];
+    has_invoice: LicenseHasProductHasInvoice[];
+    start_version?: string;
+    purchase_date?: Date;
+    expired_date?: Date;
 }
 
-export interface BaseUrl{
-  status?: number;
-  url: string;
+export interface BaseUrl {
+    status?: number;
+    url: string;
 }
 
 export interface LicenseHasProductHasUser {
-  user_id: string;
-  username: string;
+    user_id: string;
+    username: string;
 }
 
 export interface LicenseHasProductHasInvoice {
-  description: string;
-  transaction_id: string;
-  amount: number;
-  payment_method: string;
-  status: number;
-  purchased_date: Date;
+    description: string;
+    transaction_id: string;
+    amount: number;
+    payment_method: string;
+    status: number;
+    purchased_date: Date;
 }
