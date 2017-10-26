e398da5a981f43fc4c2a37396aae17a150539d7c
khoild <khoild@smartosc.com>
2017-10-26 14:27:09 +0700

    - remove console

--- server/modules/retail/methods/pricing/create_pricing.ts
+++ server/modules/retail/methods/pricing/create_pricing.ts
@@ -5,23 +5,22 @@ import {Role} from "../../../account/api/role";
 import {Price} from "../../models/price";
 
 new ValidatedMethod({
-  name: "pricing.create_pricing",
-  validate: function () {
-    const user = OM.create<User>(User).loadById(this.userId);
-    if (user.isInRoles([Role.SUPERADMIN, Role.ADMIN, Role.SALES], Role.GROUP_CLOUD)) {
-    } else {
-      throw new Meteor.Error("pricing.create_pricing_error", "Access denied");
-    }
-  },
-  run: function (data: Object) {
-      console.log(data);
-    let defer = $q.defer();
-
-    let pricingModel = OM.create<Price>(Price);
-
-    pricingModel.addData(data['pricing'])
-                .save()
-                .then(() => defer.resolve(), (err) => defer.reject(err));
-    return defer.promise;
-  }
-});
+                        name: "pricing.create_pricing",
+                        validate: function () {
+                            const user = OM.create<User>(User).loadById(this.userId);
+                            if (user.isInRoles([Role.SUPERADMIN, Role.ADMIN, Role.SALES], Role.GROUP_CLOUD)) {
+                            } else {
+                                throw new Meteor.Error("pricing.create_pricing_error", "Access denied");
+                            }
+                        },
+                        run: function (data: Object) {
+                            let defer = $q.defer();
+        
+                            let pricingModel = OM.create<Price>(Price);
+        
+                            pricingModel.addData(data['pricing'])
+                                        .save()
+                                        .then(() => defer.resolve(), (err) => defer.reject(err));
+                            return defer.promise;
+                        }
+                    });
