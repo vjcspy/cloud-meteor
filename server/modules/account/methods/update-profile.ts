750ecbb3efc2b5d43d1acb99c8566ca91b3f7599
khoild <khoild@smartosc.com>
2017-12-12 16:43:58 +0700

    - done update account info

--- server/modules/account/methods/update-profile.ts
+++ server/modules/account/methods/update-profile.ts
@@ -2,22 +2,34 @@ import {OM} from "../../../code/Framework/ObjectManager";
 import {User} from "../models/user";
 
 new ValidatedMethod({
-                      name: "accounts.user_update_profile",
-                      validate: function (profile: any) {
-                        if (!this.userId) throw new Meteor.Error('unauthorized',
-                                                                 'User must be logged-in to edit profile');
-                      },
-                      run: function (profile: Object) {
-                        let user           = OM.create<User>(User).loadById(this.userId);
-                        profile['picture'] = "https://randomuser.me/api/portraits/thumb/men/" + Math.round(Math.random() * 100) + ".jpg";
-                        return user.setData('profile', profile)
-                                   .save();
-                      }
+                        name: "accounts.user_update_profile",
+                        validate: function () {
+                            if (!this.userId) throw new Meteor.Error('unauthorized',
+                                                                     'User must be logged-in to edit profile');
+                        },
+                        run: function (userData: Object) {
+                            return new Promise((resolve, reject) => {
+                                let user = OM.create<User>(User).loadById(this.userId);
+            
+                                if (!!userData['password']) {
+                                    Accounts.setPassword(user.getId(), userData['password'], {logout: false});
+                                }
+            
+                                user.setData('profile', Object.assign({}, user.getProfile(),
+                                                                      {
+                                                                          first_name: userData['profile']['first_name'],
+                                                                          last_name: userData['profile']['last_name'],
+                                                                          phone: userData['profile']['phone'],
+                                                                      }))
+                                    .save()
+                                    .then(() => resolve(), (err) => reject(err));
+                            });
+                        }
                     });
 DDPRateLimiter.addRule({
-                         userId: function (userId) {
-                           return true;
-                         },
-                         type: "method",
-                         name: "user.chats.user_send_message",
+                           userId: function (userId) {
+                               return true;
+                           },
+                           type: "method",
+                           name: "user.chats.user_send_message",
                        }, 1, 1000);
