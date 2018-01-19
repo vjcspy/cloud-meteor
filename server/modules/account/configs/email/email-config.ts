b47616ab10865ad8a249fc51eb01ed65f150c83a
Tuan Dao Van <tuandv@smartosc.com>
2018-01-19 17:14:21 +0700

    fixbug/CRCP-63 send mail to user when admin creating new shop owner

--- server/modules/account/configs/email/email-config.ts
+++ server/modules/account/configs/email/email-config.ts
@@ -24,24 +24,24 @@ Accounts.urls.enrollAccount = function (token) {
 function prepareTextEmail(user, status) {
     if (status == "resetpwd") {
         greetVar      = `Hi ${getUserName(user)},`;
-        welcomeMsgVar = "Someone recently requested a password change for your Connect-POS account. If this was you, you can set a new password here: ";
+        welcomeMsgVar = "Someone recently requested a password change for your Connect-POS account.<br>If this was you, you can set a new password here: ";
         btnTextVar    = "Reset Password";
-        beforeMsgVar  = "If you don't want to change your password or didn't request this, just ignore and delete this message.";
+        beforeMsgVar  = "If you don't want to change your password or didn't request this,<br>just ignore and delete this message.";
         regardVar     = "Thank you so much!<br>ConnectPOS Team.";
         followMsgVar  = "ConnectPOS Team.";
     } else if (status == "verify") {
         greetVar      = "Hello ";
         welcomeMsgVar = "Welcome to ConnectPOS!<br>Your account has just been created.<br>Please click the link below to varify your account.";
         btnTextVar    = "Verify your account";
-        beforeMsgVar  = "To protect the security of your account, this link will expire in 48 hours.<br>If you did not request this, please let us" +
+        beforeMsgVar  = "To protect the security of your account,<br> this link will expire in 48 hours.<br>If you did not request this,<br>please let us" +
                         " know immediately by replying to this email.";
         regardVar     = "Thank you so much!<br>ConnectPOS Team.";
         followMsgVar  = "ConnectPOS Team.";
     } else if (status == "enroll") {
         greetVar      = `Hi ${user['username']},`;
-        welcomeMsgVar = "We have just created an account for you, if it was you click the button above.";
+        welcomeMsgVar = "We have just created an account for you,<br>if it was you click the button above.";
         btnTextVar    = "Enroll";
-        beforeMsgVar  = "If you don't want to change your password or didn't request this, just ignore and delete this message.";
+        beforeMsgVar  = "If you don't want to change your password or didn't request this,<br>just ignore and delete this message.";
         regardVar     = "Thanks, SmartOSC.<br>ConnectPOS Team.";
         followMsgVar  = "Follow us on social networks";
     }
@@ -65,10 +65,10 @@ function buildEmailHtml(status) {
                <tbody>
                 <tr>
                     <td style="padding: 20px 0 30px 0;">
-                    <table style="border-collapse: collapse; box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);" border="0" width="60%" cellspacing="0" cellpadding="0" align="center">
+                    <table style="border-collapse: collapse; box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);" border="0" width="100%" cellspacing="0" cellpadding="0" align="center">
                         <tbody>
                             <tr>
-                                <td style="padding: 10px 0 10px 0;" align="center" bgcolor="#11b4a8"><h3 style="color:#fff;">${subject}</h3></td>
+                                <td style="padding: 10px 0 10px 25px;" align="left" bgcolor="#ffffff"><img src="http://account.xcloud.smartosc.com/assets/img/account/new_logo_resized.png"></td>
                             </tr>
                             <tr>
                                 <td style="padding: 10px 30px 10px 30px;" bgcolor="#ffffff">
@@ -81,7 +81,7 @@ function buildEmailHtml(status) {
                                               <td style="padding: 15px 0 10px 0; font-family: Arial, sans-serif;">${welcomeMsgVar}</td>
                                           </tr>
                                           <tr>
-                                              <td style="padding: 20px 0 20px 0; font-family: Arial, sans-serif;"><div align="center"><a style="background-color: #11b4a8; color: white; text-align: center; padding: 15px 30px; text-decoration: none;" href="${url}">${btnTextVar}</a></div></td>
+                                              <td style="padding: 20px 0 20px 0; font-family: Arial, sans-serif;"><div align="left"><a style="background-color: #11b4a8; color: white; text-align: center; padding: 15px 30px; text-decoration: none;" href="${url}">${btnTextVar}</a></div></td>
                                           </tr>
                                           <tr>
                                               <td style="padding: 0 0 0 0; font-family: Arial, sans-serif;"><p>${beforeMsgVar} <br /> ${regardVar}</p></td>
