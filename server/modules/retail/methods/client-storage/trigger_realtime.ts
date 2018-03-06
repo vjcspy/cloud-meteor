d4f42e3c9d271aadf6ea3c5a6208e946cb0251f1
khoild <khoild@smartosc.com>
2018-03-06 14:47:41 +0700

    - check license and status when trigger realtime data

--- server/modules/retail/methods/client-storage/trigger_realtime.ts
+++ server/modules/retail/methods/client-storage/trigger_realtime.ts
@@ -1,38 +1,58 @@
 import * as _ from "lodash";
 import {OM} from "../../../../code/Framework/ObjectManager";
 import {ClientStorage} from "../../models/clientstorage";
+import {License} from "../../models/license";
 
 new ValidatedMethod({
-                        name: "client.trigger_realtime",
-                        validate: function (data) {
-                        },
-                        run: function (data) {
-                            let getClientStorageModel = (): ClientStorage => OM.create<ClientStorage>(ClientStorage);
-        
-                            const clientStorageModel: ClientStorage = getClientStorageModel();
-        
-                            const addToClientStorage = (_d) => {
-                                if (!_d.hasOwnProperty('license')) {
-                                    throw new Meteor.Error("client.trigger_realtime", "can_not_find_license");
-                                }
-                                clientStorageModel.addData(_d)
-                                                  .save();
-                            };
-        
-                            if (data.hasOwnProperty('batch')) {
-                                _.forEach(data['batch'], datum => {
-                                    addToClientStorage(datum);
-                                });
-                            } else {
-                                addToClientStorage(data);
-                            }
-                        }
-                    });
+    name: "client.trigger_realtime",
+    validate: data => {
+    },
+    run: (data) => {
+        let getClientStorageModel = (): ClientStorage => OM.create<ClientStorage>(ClientStorage);
+
+        const clientStorageModel: ClientStorage = getClientStorageModel();
+
+        let passLicense;
+        const addToClientStorage = (_d) => {
+            if (!_d.hasOwnProperty('license')) {
+                throw new Meteor.Error("client.trigger_realtime", "can_not_find_license");
+            }
+            if (typeof passLicense === 'undefined') {
+                const licenseKey   = _d['license'];
+                const licenseModel = OM.create<License>(License);
+                licenseModel.load(licenseKey, 'key');
+                if (licenseModel.getId()) {
+                    if (!isNaN(licenseModel.getStatus()) && parseInt(licenseModel.getStatus() + '') === 0) {
+                        passLicense = false;
+                        throw new Meteor.Error("client.trigger_realtime", "license_has_been_disabled");
+                    } else {
+                        passLicense = true;
+                    }
+                } else {
+                    passLicense = false;
+                    throw new Meteor.Error("client.trigger_realtime", "can_not_find_license");
+                }
+            }
+
+            clientStorageModel.addData(_d)
+                              .save();
+        };
+
+        if (data.hasOwnProperty('batch')) {
+            for (let i = 0; i < data['batch'].length; i++) {
+                addToClientStorage(data['batch'][i]);
+            }
+
+        } else {
+            addToClientStorage(data);
+        }
+
+        return {result: "success", passLicense}
+    }
+});
 DDPRateLimiter.addRule({
-                           userId: function (userId) {
-                               return true;
-                           },
-                           type: "method",
-                           name: "client.trigger_realtime",
-                       }, 3, 1000);
+    userId: userId => true,
+    type: "method",
+    name: "client.trigger_realtime",
+}, 3, 1000);
 
