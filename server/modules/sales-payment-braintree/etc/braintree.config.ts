f760bd92f6c485ef48cb9f9f256e16032dd85ab1
khoild <khoild@smartosc.com>
2018-03-23 14:21:50 +0700

    - add product braintree

--- server/modules/sales-payment-braintree/etc/braintree.config.ts
+++ server/modules/sales-payment-braintree/etc/braintree.config.ts
@@ -7,6 +7,11 @@ export const BraintreeConfig = {
         publicKey: 'ty5t38gh52s5w2qs',
         privateKey: 'ec91ef68c14852d9b084ce604f8752e1',
     },
+    production: {
+        merchantId: 'xhn3rb92fxtq7rbs',
+        publicKey: 'r43bqndn4tcj5rbr',
+        privateKey: 'e15ebfb05956c2a6d3f635a547ba0a0a',
+    },
     subscription: {
         discountId: 'subscription_discount',
         linkPricingPlan: [
@@ -24,11 +29,18 @@ export const BraintreeConfig = {
     }
 };
 
+// export const BraintreeGateway = braintree.connect({
+//     environment: braintree.Environment.Sandbox,
+//     merchantId: BraintreeConfig.sandbox.merchantId,
+//     publicKey: BraintreeConfig.sandbox.publicKey,
+//     privateKey: BraintreeConfig.sandbox.privateKey
+// });
+
 export const BraintreeGateway = braintree.connect({
-                                                      environment: braintree.Environment.Sandbox,
-                                                      merchantId: BraintreeConfig.sandbox.merchantId,
-                                                      publicKey: BraintreeConfig.sandbox.publicKey,
-                                                      privateKey: BraintreeConfig.sandbox.privateKey
-                                                  });
+    environment: braintree.Environment.Production,
+    merchantId: BraintreeConfig.production.merchantId,
+    publicKey: BraintreeConfig.production.publicKey,
+    privateKey: BraintreeConfig.production.privateKey
+});
 
 BraintreeGateway.config.timeout = 10000;
