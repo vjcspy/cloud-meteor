b963f50b4221eacd22e4c4574728e3dc7085080e
KhanhLD <khanhld@smartosc.com>
2018-05-08 15:01:09 +0700

    add Manage Credit to User Managment

--- server/modules/user-credit/methods/save.ts
+++ server/modules/user-credit/methods/save.ts
@@ -1,5 +1,4 @@
 import * as $q from "q";
-import * as _ from "lodash";
 import {OM} from "../../../code/Framework/ObjectManager";
 import {User} from "../../account/models/user";
 import {Role} from "../../account/models/role";
