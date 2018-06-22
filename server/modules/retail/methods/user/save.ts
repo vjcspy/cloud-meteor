5fdf7cfce624540972d21d460d734c8c98364d90
HarryNguyen <hoangnc@smartosc.com>
2018-06-22 11:25:34 +0700

    save user by agency

--- server/modules/retail/methods/user/save.ts
+++ server/modules/retail/methods/user/save.ts
@@ -16,6 +16,12 @@ new ValidatedMethod({
                         run: function (data: Object) {
                             let defer = $q.defer();
                             let user: User = OM.create<User>(User);
+                            let profile           = user.getProfile() || {};
+                            profile['first_name'] = data['profile']['first_name'];
+                            profile['last_name']  = data['profile']['last_name'];
+                            profile['phone']      = data['profile']['phone'];
+                            profile['country']      = data['profile']['country'];
+                            profile['created_by'] = Meteor.userId();
                             if (!!data['_id']) {
                                 if (!!data['password']) {
                                     Accounts.setPassword(data['_id'], data['password'], {logout: false});
@@ -27,26 +33,18 @@ new ValidatedMethod({
                                                                            username: data['username'],
                                                                            email: data['email'],
                                                                            password: !!data['password'] ? data['password'] : User.DEFAULT_PASSWORD_USER,
+                                                                           profile: profile
                                                                        });
                                 !data['password'] ? Accounts.sendEnrollmentEmail(newUserId) : '';
-    
                                 user.loadById(newUserId);
                             }
-    
                             if (!user.getId()) {
                                 throw new Meteor.Error('user.save', 'can_not_find_user');
                             }
-    
                             if (user.isInRoles([Role.SUPERADMIN], Role.GROUP_CLOUD)) {
                                 throw  new Meteor.Error('user.save', 'sorry_this_method_not_support_update_super_admin_data');
                             }
-
-                            let profile           = user.getProfile() || {};
-                            profile['first_name'] = data['profile']['first_name'];
-                            profile['last_name']  = data['profile']['last_name'];
-                            profile['phone']      = data['profile']['phone'];
-                            profile['country']      = data['profile']['country'];
-                            user.setData('profile', profile)
+                            user.setData('take_care_by_agency',data['take_care_by_agency'])
                                 .setData('agency',data['agency'])
                                 .setData('customer_type', data["customer_type"])
                                 .setData('last_date_trial', data["last_date_trial"])
