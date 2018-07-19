a177d205c3cf2e6c9b425ac06ac68aff69ff281a
KhanhLD <khanhld@smartosc.com>
2018-07-19 17:49:24 +0700

    [Extend Plan] - extend other product

--- server/modules/sales/methods/plan/extend-plan.ts
+++ server/modules/sales/methods/plan/extend-plan.ts
@@ -4,6 +4,7 @@ import * as moment from "moment";
 import {Payment} from "../../../sales-payment/models/payment";
 import {Plan} from "../../models/plan";
 import {PaymentGatewayDataInterface} from "../../../sales-payment/models/payment/payment-gateway-data-interface";
+import {ProductCollection} from "../../../retail/collections/products";
 
 new ValidatedMethod({
                         name: "sales.extend_plan",
@@ -13,25 +14,34 @@ new ValidatedMethod({
                             let payment                       = OM.create <Payment>(Payment);
                             let braintree: PaymentGatewayDataInterface = {id: 'braintree'};
                             const license_id = data['license_id'];
-                            const product_id = data['product_id'];
+                            let product_id;
+                            const posProduct = ProductCollection.findOne({code: 'xpos'});
+
+                            if (data.hasOwnProperty('product_id')) {
+                                product_id = data['product_id'];
+                            } else {
+                                product_id = posProduct['_id'];
+                            }
 
                             const license = OM.create<License>(License).loadById(license_id);
-                            const userId = license.getData('shop_owner_id');
-                            let licenseHasProduct = license.getProductLicense(product_id);
-                           if (!licenseHasProduct) {
-                               return;
-                           } else {
-                               const now = moment().toDate();
-                               if (licenseHasProduct['expiry_date'] > now) {
-                                   return;
-                               } else {
-                                   const plan = OM.create<Plan>(Plan).loadById(licenseHasProduct['plan_id']);
-                                   const coupon_id = plan.getData('coupon_id');
-                                   
-                                   if (plan) {
-                                       return payment.extend(plan, braintree, product_id, userId, coupon_id);
-                                   }
-                               }
-                           }
+                            if (license) {
+                                const userId = license.getData('shop_owner_id');
+                                let licenseHasProduct = license.getProductLicense(product_id);
+                                if (!licenseHasProduct) {
+                                    return;
+                                } else {
+                                    const now = moment().toDate();
+                                    if (licenseHasProduct['expiry_date'] > now) {
+                                        return;
+                                    } else {
+                                        const plan = OM.create<Plan>(Plan).loadById(licenseHasProduct['plan_id']);
+                                        const coupon_id = plan.getData('coupon_id');
+
+                                        if (plan) {
+                                            return payment.extend(plan, braintree, product_id, userId, coupon_id);
+                                        }
+                                    }
+                                }
+                            }
                         }
                     });
