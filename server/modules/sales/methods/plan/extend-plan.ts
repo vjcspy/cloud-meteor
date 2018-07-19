97d0f179a3aaab1e268be4870d4c7930c8ae6af0
KhanhLD <khanhld@smartosc.com>
2018-07-19 17:39:47 +0700

    [Extend Plan] - extend other product

--- server/modules/sales/methods/plan/extend-plan.ts
+++ server/modules/sales/methods/plan/extend-plan.ts
@@ -1,6 +1,5 @@
 import {OM} from "../../../../code/Framework/ObjectManager";
 import {License} from "../../../retail/models/license";
-import {ProductCollection} from "../../../retail/collections/products";
 import * as moment from "moment";
 import {Payment} from "../../../sales-payment/models/payment";
 import {Plan} from "../../models/plan";
@@ -14,22 +13,23 @@ new ValidatedMethod({
                             let payment                       = OM.create <Payment>(Payment);
                             let braintree: PaymentGatewayDataInterface = {id: 'braintree'};
                             const license_id = data['license_id'];
+                            const product_id = data['product_id'];
+
                             const license = OM.create<License>(License).loadById(license_id);
-                            const posProduct = ProductCollection.findOne({code: 'xpos'});
                             const userId = license.getData('shop_owner_id');
-                            let licenseHasPos = license.getProductLicense(posProduct['_id']);
-                           if (!licenseHasPos) {
+                            let licenseHasProduct = license.getProductLicense(product_id);
+                           if (!licenseHasProduct) {
                                return;
                            } else {
                                const now = moment().toDate();
-                               if (licenseHasPos['expiry_date'] > now) {
+                               if (licenseHasProduct['expiry_date'] > now) {
                                    return;
                                } else {
-                                   const plan = OM.create<Plan>(Plan).loadById(licenseHasPos['plan_id']);
+                                   const plan = OM.create<Plan>(Plan).loadById(licenseHasProduct['plan_id']);
                                    const coupon_id = plan.getData('coupon_id');
                                    
                                    if (plan) {
-                                       return payment.extend(plan, braintree, posProduct['_id'], userId, coupon_id);
+                                       return payment.extend(plan, braintree, product_id, userId, coupon_id);
                                    }
                                }
                            }
