3e1c24bd8bb9b1c6444890e979d3c135a51c4ef4
KhanhLD <khanhld@smartosc.com>
2018-07-23 15:37:46 +0700

    [Storages]

--- server/modules/retail/methods/client-storage/get_storage.ts
+++ server/modules/retail/methods/client-storage/get_storage.ts
@@ -1,11 +1,8 @@
-import * as _ from "lodash";
 import {OM} from "../../../../code/Framework/ObjectManager";
-import {ClientStorage} from "../../models/clientstorage";
 import {User} from "../../../account/models/user";
 import {Role} from "../../../account/models/role";
-import * as $q from "q";
 import {ClientStoragesCollection} from "../../collections/clientstorages";
-
+import * as _ from "lodash";
 new ValidatedMethod({
                         name: "client-storage.get_storage",
                         validate: function () {
@@ -20,39 +17,43 @@ new ValidatedMethod({
                             let storages = [];
                             if ((!data['licenses'] || data['licenses'].length === 0) && !data['baseUrl'] && !data['startTime'] && !data['endTime']) {
                                 storages = ClientStoragesCollection.collection.aggregate([
-                                    { $group: { _id: "$license", total: { $sum: 1} }  },
+                                    { $group: { _id: {license: "$license", base_url: "$base_url"}, records: { $sum: 1} }  },
                                 ]);
-                                console.log()
                             } else if ((!data['licenses'] || data['licenses'].length === 0) && !data['baseUrl'] && data['startTime'] && data['endTime']) {
-                                storages = ClientStoragesCollection.find({created_at: {$gte: data['startTime'], $lte: data['endTime']}}).fetch();
+                                storages = ClientStoragesCollection.collection.aggregate([
+                                    { $match: {created_at: {$gte: data['startTime'], $lte: data['endTime']}}},
+                                    { $group: { _id: {license: "$license", base_url: "$base_url"}, records: { $sum: 1} }  },
+                                ]);
                             } else if ((!data['licenses'] || data['licenses'].length === 0) && data['baseUrl'] && data['startTime'] && data['endTime']) {
-                                storages = ClientStoragesCollection.find({base_url: data['baseUrl'], created_at: {$gte: data['startTime'], $lte: data['endTime']}}).fetch();
+                                storages = ClientStoragesCollection.collection.aggregate([
+                                    { $match: {base_url: data['baseUrl'], created_at: {$gte: data['startTime'], $lte: data['endTime']}}},
+                                    { $group: { _id: {license: "$license", base_url: "$base_url"}, records: { $sum: 1} }  },
+                                ]);
                             } else if ((data['licenses'] && data['licenses'].length > 0) && !data['baseUrl'] && data['startTime'] && data['endTime']) {
-                                storages = ClientStoragesCollection.find({license: {$in: data['licenses']}, created_at: {$gte: data['startTime'], $lte: data['endTime']}}).fetch();
+                                storages = ClientStoragesCollection.collection.aggregate([
+                                    { $match: {license: {$in: data['licenses']}, created_at: {$gte: data['startTime'], $lte: data['endTime']}}},
+                                    { $group: { _id: {license: "$license", base_url: "$base_url"}, records: { $sum: 1} }  },
+                                ]);
                             } else {
-                                storages = ClientStoragesCollection.find({license: {$in: data['licenses']}, base_url: data['baseUrl'], created_at: {$gte: data['startTime'], $lte: data['endTime']}}).fetch();
+                                storages = ClientStoragesCollection.collection.aggregate([
+                                    { $match: {license: {$in: data['licenses']}, base_url: data['baseUrl'], created_at: {$gte: data['startTime'], $lte: data['endTime']}}},
+                                    { $group: { _id: {license: "$license", base_url: "$base_url"}, records: { $sum: 1} }  },
+                                ]);
                             }
                             
                             if (!storages) {
                                 throw new Meteor.Error("storage.error_edit", "Storage Not Found");
                             }
-                            
-                            // const dataGroup = _.groupBy(storages, 'license');
-                            // _.forEach(dataGroup, (value, key) => {
-                            //     dataResolve.push({
-                            //         license: key,
-                            //         url: [],
-                            //         startTime: data['startTime'] ? data['startTime'] : null,
-                            //         endTime: data['endTime'] ? data['endTime'] : null
-                            //     });
-                            //     _.forEach(_.countBy(value, 'base_url'), (records, base_url) => {
-                            //         dataResolve[dataResolve.length -1]['url'].push({
-                            //             base_url: base_url,
-                            //             records: records,
-                            //         });
-                            //     })
-                            //
-                            // });
-                            return ;
+
+                            const dataGroup = _.groupBy(storages, '_id.license');
+                            _.forEach(dataGroup, (value, key) => {
+                                dataResolve.push({
+                                    license: key,
+                                    url: value,
+                                    startTime: data['startTime'] ? data['startTime'] : null,
+                                    endTime: data['endTime'] ? data['endTime'] : null
+                                });
+                            });
+                            return dataResolve;
                         }
                     });
