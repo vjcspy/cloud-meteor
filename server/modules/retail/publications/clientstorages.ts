13a5e436dbd6aaa3945b88bf95fbf7626121aae3
khoild <khoild@smartosc.com>
2018-07-26 14:57:25 +0700

    - revert code

--- server/modules/retail/publications/clientstorages.ts
+++ server/modules/retail/publications/clientstorages.ts
@@ -4,21 +4,24 @@ import {Role} from "../../account/models/role";
 import {License} from "../models/license";
 import {ClientStoragesCollection} from "../collections/clientstorages";
 import * as _ from "lodash";
-import * as moment from "moment";
-import {DateTimeHelper} from "../../../code/Framework/DateTimeHelper";
-import {ClientStorageInterface} from "../api/client_storage-interface";
 
-Meteor.publishComposite('client_storages', function (): PublishCompositeConfig<ClientStorageInterface> {
-    
+Meteor.publish('client_storages', function () {
     if (!this.userId)
         return;
     const user: User = OM.create<User>(User).loadById(this.userId);
-    if (user.isInRoles([Role.SUPERADMIN, Role.ADMIN], Role.GROUP_CLOUD)) {
-        return {
-            find: () => {
-                return ClientStoragesCollection.collection.find({});
-            },
-        };
+    if (user.isInRoles(Role.USER)) {
+        const licenses = user.getLicenses();
+        if (_.size(licenses) == 1) {
+            const license: License = OM.create<License>(License).loadById(licenses[0].license_id);
+            if (license) {
+                return ClientStoragesCollection.collection.find({license: license.getData('key')});
+            }
+            else {
+                return;
+            }
+        }
+        else {
+            return;
+        }
     } else { return; }
-    
 });
