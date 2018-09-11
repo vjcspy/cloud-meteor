f305ae9e9d65ce22352231c82e2a397db906fdc5
KhanhLD <khanhld@smartosc.com>
2018-09-11 16:30:30 +0700

    [config list Email]

--- server/modules/retail/jobs/update-expire-date.ts
+++ server/modules/retail/jobs/update-expire-date.ts
@@ -24,36 +24,53 @@ SyncedCron.add({
                     let expire_date: any[] =[];
                     const licenses = LicenseCollection.find().fetch();
                     const pricingCollection = PricingCollection.find().fetch();
+                    const listExpires = ExpireDateCollection.find().fetch();
                     const user      = OM.create<User>(User);
                     let expire = OM.create<Expiredate>(Expiredate);
-                    ExpireDateCollection.remove({});
+                    // ExpireDateCollection.remove({});
                    _.forEach(licenses, (l) => {
                         if (!!l['shop_owner_id'] && l['status'] == 1){
                             user.loadById(l['shop_owner_id']);
 
                             _.forEach(l['has_product'], (h) => {
-                                let pricing = _.find(pricingCollection, (p) => p['_id']==h['pricing_id']);
+                                const exist = _.find(listExpires, le => le['license_id'] === l['_id'] && le['product_id'] === h['product_id']);
                                 let expireDate  = moment(h['expiry_date'], 'YYYY-MM-DD');
                                 let currentTime = moment(DateTimeHelper.getCurrentDate(), 'YYYY-MM-DD');
                                 let diff        = expireDate.diff(currentTime,'days');
                                 if (h['status'] == 1 && diff < 3) {
-                                    expire_date.push({
-                                        license_id : l['_id'],
-                                        email: user.getEmail(),
-                                        shop_owner_username: user.getUsername(),
-                                        product_id: h['product_id'],
-                                        purchase_date: h['purchase_date'],
-                                        expiry_date: h['expiry_date'],
-                                        plan_id: h['plan_id'],
-                                        pricing_code: pricing['code'],
-                                        pricing_id: pricing['_id']
-                                    });
+                                    if(!exist) {
+                                        let pricing = _.find(pricingCollection, (p) => p['_id']==h['pricing_id']);
+                                        const newExp = {
+                                            license_id : l['_id'],
+                                            email: user.getEmail(),
+                                            shop_owner_username: user.getUsername(),
+                                            product_id: h['product_id'],
+                                            purchase_date: h['purchase_date'],
+                                            expiry_date: h['expiry_date'],
+                                            plan_id: h['plan_id'],
+                                            pricing_code: pricing['code'],
+                                            pricing_id: pricing['_id']
+                                        };
+                                        expire.addData(newExp).save();
+                                    } else {
+                                        expire.loadById(exist['_id']);
+                                        expire.setData('purchase_date', h['purchase_date'])
+                                              .setData('expiry_date', h['expiry_date'])
+                                              .setData('plan_id', h['plan_id'])
+                                              .setData('pricing_code', h['pricing_code'])
+                                              .setData('pricing_id', h['pricing_id'])
+                                              .save();
+                                    }
 
+                                } else {
+                                    if (exist) {
+                                        expire.loadById(exist['_id']);
+                                        expire.remove();
+                                    }
                                 }
                             })
                         }
                     });
-                   expire.createrExpireDate(expire_date).then(()=>{},(e)=>{console.log(e)});
                 };
                export const sendEmailExpireDate = () => {
                    const expireDate = ExpireDateCollection.find().fetch();
@@ -72,7 +89,7 @@ SyncedCron.add({
                               }
                           }
                       });
-                   if(listExp.length > 0 && BRAINTREE_ENVIROMENT !== 'SANDBOX') {
+                   if(listExp.length > 0 && BRAINTREE_ENVIROMENT === 'SANDBOX') {
                        let sendData = {
                            listUser: listExp
                        };
