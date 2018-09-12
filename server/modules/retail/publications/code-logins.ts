874cff58e148c1f6bf0a644b9cbfddc5e14dd564
KhanhLD <khanhld@smartosc.com>
2018-09-12 15:39:58 +0700

    [shop owner can update pincode barcode cashier]

--- server/modules/retail/publications/code-logins.ts
+++ server/modules/retail/publications/code-logins.ts
@@ -1,12 +1,31 @@
 import {CodeLoginInterface} from "../api/code-login-interface";
 import {CodeLoginsCollection} from "../collections/code-login-collection";
+import {OM} from "../../../code/Framework/ObjectManager";
+import {User} from "../../account/models/user";
+import {License} from "../models/license";
+import {UserHasLicense} from "../../account/api/user-interface";
+import {Role} from "../../account/models/role";
+import * as _ from "lodash";
 Meteor.publishComposite("code_login", function (): PublishCompositeConfig<CodeLoginInterface> {
     if (!this.userId) {
         return;
     }
-    return {
-        find() {
-            return CodeLoginsCollection.collection.find({user_id: Meteor.userId()});
+    let userModel: User = OM.create<User>(User).loadById(this.userId);
+    if (userModel.isInRoles(Role.USER)) {
+        let license: UserHasLicense[] = userModel.getLicenses();
+        if (_.isArray(license)
+            && _.size(license) == 1
+            && _.indexOf([User.LICENSE_PERMISSION_OWNER, User.LICENSE_PERMISSION_CASHIER], license[0].license_permission) > -1) {
+            return {
+                find: () => {
+                    return CodeLoginsCollection.collection.find({license_id: license[0].license_id});
+                }
+            }
         }
-    };
+    } else {
+        return {
+            find() {
+                return CodeLoginsCollection.collection.find({user_id: Meteor.userId()});
+            }
+        };}
 });
