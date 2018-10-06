380830d5926a9d5651bc121ea3c7887be2525761
Hung Nguyen Huy <hunguetit@gmail.com>
2018-10-06 14:24:52 +0700

    refactor

--- server/modules/retail/methods/second-screen/get_screen.ts
+++ server/modules/retail/methods/second-screen/get_screen.ts
@@ -9,19 +9,28 @@ new ValidatedMethod({
     validate: function () {
     },
     run: function (data: Object) {
+        let defer = $q.defer();
         let secondScreenModel: SecondScreen = OM.create<SecondScreen>(SecondScreen);
         let secondScreen = SecondScreenCollection.findOne({license_key: data['license_key'], url: data['url'], register_id: data['register_id'], user_id: this.userId});
         let cartModel: Cart = OM.create<Cart>(Cart);
         if (secondScreen){
             return secondScreen['cart_id'];
         } else {
-            cartModel.addData({customer_name:'Guest Customer'}).save()
-                .then(() => {
-                    secondScreenModel.addData({license_key: data['license_key'], url: data['url'], register_id: data['register_id'], user_id: this.userId, cart_id: cartModel.getId()}).save()
-                        .then(() => {
-                            return secondScreenModel.getData('cart_id');
-                        });
-                });
+            cartModel.addData({customer_name: 'Guest Customer'}).save()
+                     .then(() => {
+                         secondScreenModel.addData({
+                                                       license_key: data['license_key'],
+                                                       url: data['url'],
+                                                       register_id: data['register_id'],
+                                                       user_id: this.userId,
+                                                       cart_id: cartModel.getId()
+                                                   }).save()
+                                          .then(() => {
+                                              return defer.resolve(secondScreenModel.getData('cart_id'));
+                                          })
+                                          .catch((err) => defer.reject(err));
+                     });
         }
+        return defer.promise;
     }
 });
