23aba45b17e8eeadfa51a9f41b38e489bafa3e06
vjcspy <mr.vjcspy@gmail.com>
2018-12-05 16:30:38 +0700

    - update meteor version

--- server/modules/retail/jobs/clear-client-storage.ts
+++ server/modules/retail/jobs/clear-client-storage.ts
@@ -4,25 +4,25 @@ import {ClientStoragesCollection} from "../collections/clientstorages";
 import {DateTimeHelper} from "../../../code/Framework/DateTimeHelper";
 
 SyncedCron.add({
-                   name: "Remove old clientstorages(larger than 5 days)",
+                   name: "Remove old clientstorages",
                    schedule: function (parser) {
                        return parser.text('every 1 day');
                    },
                    job: function () {
-                       // deleteClientStorage();
+                       deleteClientStorage();
                    }
                });
 
-// const deleteClientStorage = () => {
-//     const allClientStorages = ClientStorages.find().fetch();
-//     if (allClientStorages.length > 0) {
-//         _.forEach(allClientStorages, (clientStorage) => {
-//             let createTime  = moment(clientStorage['created_at'], 'YYYY-MM-DD');
-//             let currentTime = moment(DateTimeHelper.getCurrentDate(), 'YYYY-MM-DD');
-//             let diff        = createTime.diff(currentTime, 'days');
-//             if (diff > 5) {
-//                 ClientStorages.remove(clientStorage);
-//             }
-//         });
-//     }
-// };
+const deleteClientStorage = () => {
+    const allClientStorages = ClientStoragesCollection.find().fetch();
+    if (allClientStorages.length > 0) {
+        _.forEach(allClientStorages, (clientStorage) => {
+            let createTime  = moment(clientStorage['created_at'], 'YYYY-MM-DD');
+            let currentTime = moment(DateTimeHelper.getCurrentDate(), 'YYYY-MM-DD');
+            let diff        = createTime.diff(currentTime, 'days');
+            if (diff > 1) {
+                ClientStoragesCollection.remove(clientStorage);
+            }
+        });
+    }
+};
