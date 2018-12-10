dff673c6f5cc8899dfe49f71b34bf2a72fbedce3
vjcspy <mr.vjcspy@gmail.com>
2018-12-10 10:34:56 +0700

    - add support docker

--- server/modules/retail/jobs/clear-client-storage.ts
+++ server/modules/retail/jobs/clear-client-storage.ts
@@ -1,7 +1,6 @@
-import * as _ from "lodash";
 import * as moment from 'moment';
 import {ClientStoragesCollection} from "../collections/clientstorages";
-import {DateTimeHelper} from "../../../code/Framework/DateTimeHelper";
+import {StoneLogger} from "../../../code/core/logger/logger";
 
 SyncedCron.add({
                    name: "Remove old clientstorages",
@@ -14,15 +13,6 @@ SyncedCron.add({
                });
 
 const deleteClientStorage = () => {
-    const allClientStorages = ClientStoragesCollection.find().fetch();
-    if (allClientStorages.length > 0) {
-        _.forEach(allClientStorages, (clientStorage) => {
-            let createTime  = moment(clientStorage['created_at'], 'YYYY-MM-DD');
-            let currentTime = moment(DateTimeHelper.getCurrentDate(), 'YYYY-MM-DD');
-            let diff        = createTime.diff(currentTime, 'days');
-            if (diff > 1) {
-                ClientStoragesCollection.remove(clientStorage);
-            }
-        });
-    }
+    StoneLogger.info("Clear trash");
+    ClientStoragesCollection.remove({created_at: {$lt: moment(new Date()).subtract(1, 'days').format("DD/MM/YYYY")}})
 };
