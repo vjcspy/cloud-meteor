3740a1eb733e68151c4e18404df1df3d0be1bb4e
vjcspy <mr.vjcspy@gmail.com>
2018-12-12 10:26:43 +0700

    - minor error

--- server/modules/retail/jobs/clear-client-storage.ts
+++ server/modules/retail/jobs/clear-client-storage.ts
@@ -5,7 +5,7 @@ import {StoneLogger} from "../../../code/core/logger/logger";
 SyncedCron.add({
                    name: "Remove old clientstorages",
                    schedule: function (parser) {
-                       return parser.text('every 24 hours');
+                       return parser.text('every 1 minutes');
                    },
                    job: function () {
                        deleteClientStorage();
@@ -14,5 +14,5 @@ SyncedCron.add({
 
 const deleteClientStorage = () => {
     StoneLogger.info("Clear trash");
-    ClientStoragesCollection.remove({created_at: {$lt: moment(new Date()).subtract(1, 'days').format("DD/MM/YYYY")}})
+    ClientStoragesCollection.remove({created_at: {$lt: moment(new Date()).subtract(1, 'days').toDate()}})
 };
